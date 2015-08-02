fbd0ebd316f30706dc762f2bd061984aeb2b1a90
kakira <kurokuro917@gmail.com>
2015-08-02 20:07:30 +0900

    JS-3

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,7 @@
 // 課題 JS-3 の実装をここに記述してください。
+
+function displayTable() {
+	var div = document.getElementById('table-container');
+	var loginput = document.getElementById('log-input');
+	createLogTable(div, parseLTSVLog(loginput.value));
+}
