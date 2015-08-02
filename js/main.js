1d88a81347cbffffa58d97dc4c63f487766d5fb7
kakira <kurokuro917@gmail.com>
2015-08-02 19:42:39 +0900

    JS-1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 // 課題 JS-1: 関数 `parseLTSVLog` を記述してください
 
-// 課題 JS-2: 関数 `createLogTable` を記述してください
+function parseLTSVLog(logStr) {
+	var logs = logStr.split(/\r\n|\r|\n/);
+	var ret = new Array();
+	for(var i = 0; i < logs.length; i++) {
+		if (logs[i] == "") continue;
+		var columns = logs[i].split(/\t/);
+		var object = { };
+		for(var j = 0; j < columns.length; j++) {
+			var data = columns[j].match(/(.*):(.*)$/);
+			var key = data[1];
+			var value = data[2];
+			if (!isNaN(parseInt(value))) {
+				value = parseInt(value);
+			}
+			object[key] = value;
+		}
+		ret.push(object);
+	}
+	return ret;
+}
